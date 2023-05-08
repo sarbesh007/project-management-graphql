47c3a7600a2eecfda57c55be08cbe197975e4b37
Sarvesh Kumar <sarveshkumar@Sarveshs-MacBook-Pro.local>
2023-05-09 00:53:55 +0530

    Added server url

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: "https://project-management-graphql-ksmx.onrender.com/graphql",
   cache,
 });
 
